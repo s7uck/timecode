3e26a57606d75ae8b5e1d7c59f489587dbcad027
stuck <aireportyt@gmail.com>
2024-04-28 11:35:24 +0200

    Return timecode string * => normal beep _ => low beep - => short beep . => short low beep

--- timecode.py
+++ timecode.py
@@ -28,59 +28,72 @@ def signal_terminate():
     sleep(0.5)
 
 def write_time(time):
-	hour = time.hour
-	minute = time.minute
-	second = time.second
+    hour = time.hour
+    minute = time.minute
+    second = time.second
 
-	print(f"{hour > 12} {hour}:{minute}:{second}")
+    string = ""
 
-	if hour == 0:
-		beep(low_beep)
-		beep(low_beep)
-		beep(low_beep)
+    print(f"{hour > 12} {hour}:{minute}:{second}")
 
-	if hour >= 7:
-		beep(short_low_beep)
-	if hour >= 12:
-		beep(short_low_beep)
-	if hour >= 20:
-		beep(short_low_beep)
+    if hour == 0:
+        beep(low_beep)
+        beep(low_beep)
+        beep(low_beep)
+        string = "___"
 
-	hour12 = hour
-	if hour > 12:
-		hour12 = hour - 12
+    if hour >= 7:
+        beep(short_low_beep)
+        string += "."
+    if hour >= 12:
+        beep(short_low_beep)
+        string += "."
+    if hour >= 20:
+        beep(short_low_beep)
+        string += "."
 
-	minute12 = math.floor(minute / 5)
-	minuteOffset = int(str(minute)[-1]) #sorry i couldn't figure out a better way right now
+    hour12 = hour
+    if hour > 12:
+        hour12 = hour - 12
 
-	second12 = math.floor(second / 5)
-	secondOffset = int(str(second)[-1])
+    minute12 = math.floor(minute / 5)
+    minuteOffset = int(str(minute)[-1]) #sorry i couldn't figure out a better way right now
 
-	print(hour12)
-	for h in range(hour12):
-		beep(normal_beep)
+    second12 = math.floor(second / 5)
+    secondOffset = int(str(second)[-1])
 
-	blanking_interval()
+    print(hour12)
+    for h in range(hour12):
+        beep(normal_beep)
+        string += "*"
 
-	print(minute12)
-	for m in range(minute12):
-		beep(normal_beep)
-	print(minuteOffset)
-	for n in range(minuteOffset):
-		beep(short_beep)
+    blanking_interval()
+    string += " "
 
-	blanking_interval()
+    print(minute12)
+    for m in range(minute12):
+        beep(normal_beep)
+        string += "*"
+    print(minuteOffset)
+    for n in range(minuteOffset):
+        beep(short_beep)
+        string += "-"
 
-	print(second12)
-	for s in range(second12):
-		beep(normal_beep)
-	print(secondOffset)
-	for t in range(secondOffset):
-		beep(short_beep)
+    blanking_interval()
 
-	blanking_interval()
-	signal_terminate()
-	return
+    print(second12)
+    for s in range(second12):
+        beep(normal_beep)
+        string += "*"
+    print(secondOffset)
+    for t in range(secondOffset):
+        beep(short_beep)
+        string += "-"
+
+    blanking_interval()
+    signal_terminate()
+    string += "  "
+    return string
 
 
 def main(now=datetime.datetime.now().time()):
