c45a59b166ac996423b50532ca7f00a77a68fb6d
stuck <aireportyt@gmail.com>
2024-04-28 16:10:27 +0200

    Add time code encoding/decoding functions

--- timecode.py
+++ timecode.py
@@ -5,27 +5,98 @@ import math
 import mpv
 import datetime
 
-time_format       = '%p %H:%M:%S'
-normal_beep       = 'chime.ogg'
-short_beep        = 'short_chime.ogg'
-low_beep          = 'chime_low.ogg'
-short_low_beep    = 'short_chime_low.ogg'
-blanking_interval = 'empty.ogg'
-signal_terminate  = 'terminate.ogg'
+time_format = '%p %H:%M:%S'
+chime = 'chime.ogg'
+short_chime = 'short_chime.ogg'
+chime_low = 'chime_low.ogg'
+short_chime_low = 'short_chime_low.ogg'
+empty = 'empty.ogg'
+terminate = 'terminate.ogg'
+
+normal_beep = "*"
+short_beep = "-"
+low_beep = "_"
+short_low_beep = "."
+blank = " "
+terminate = "\\"
+
 
 player = mpv.MPV()
 
 def beep(b):
-    print(b)
     player.play(b)
     player.wait_for_playback()
 
 def blanking_interval():
-    #player.play(blanking_interval)
-    sleep(1)
+    player.play(empty)
+    player.wait_for_playback()
 def signal_terminate():
-    #player.play(signal_terminate)
-    sleep(0.5)
+    player.play(terminate)
+    player.wait_for_playback()
+
+def parse_timecode(s, separator=blank, hour_beep=normal_beep, minute_beep=normal_beep, minute_unit_beep=short_beep, second_beep=normal_beep, second_unit_beep=short_beep):
+    hour = 0
+    minute = 0
+    second = 0
+    daytime = 0
+
+    signals = s.split(separator)
+    for i, signal in enumerate(signals):
+        if i == 0:
+            daytime = signal.count(short_low_beep)
+            hour = signal.count(hour_beep)
+        if i == 1:
+            minute = signal.count(minute_beep) * 5 + signal.count(minute_unit_beep)
+        if i == 2:
+            second = signal.count(second_beep) * 5 + signal.count(second_unit_beep)
+    if daytime > 0: hour += 12
+
+    return datetime.time(hour, minute, second)
+
+def beep_from_string(s):
+    beeps = []
+    for symbol in s:
+        beeps.append({
+            "*": chime,
+            "-": short_chime,
+            "_": chime_low,
+            ".": short_chime_low,
+            " ": empty,
+            "\\": terminate
+        }[symbol])
+    return beeps
+
+def time_encode(time, separator=blank, hour_beep=normal_beep, minute_beep=normal_beep, minute_unit_beep=short_beep, second_beep=normal_beep, second_unit_beep=short_beep):
+    hour = time.hour
+    minute = time.minute
+    second = time.second
+
+    result = ""
+
+    if hour == 0:
+        result = 3 * low_beep
+
+    if hour >= 7: result += short_low_beep
+    if hour >= 12: result += short_low_beep
+    if hour >= 20: result += short_low_beep
+
+    hour12 = hour
+    if hour > 12: hour12 = hour - 12
+
+    minute12 = math.floor(minute / 5)
+    minuteOffset = minute - (minute12 * 5)
+
+    second12 = math.floor(second / 5)
+    secondOffset = second - (second12 * 5)
+
+    hour_beeps = hour_beep * hour12
+    minute_beeps = minute_beep * minute12 + minute_unit_beep * minuteOffset
+    second_beeps = second_beep * second12 + second_unit_beep * secondOffset
+
+    result += hour_beeps + separator + minute_beeps + separator + second_beeps + terminate
+
+    return result
+
 
 def write_time(time):
     hour = time.hour
